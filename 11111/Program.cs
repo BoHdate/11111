0a01b974a2b9e54cfa61c24a8e0046ba56b5ce3a
= <hanten2288@gmail.com>
2020-09-17 12:08:36 +0300

    dssdfssdf

--- 11111/Program.cs
+++ 11111/Program.cs
@@ -6,7 +6,7 @@ namespace _11111
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello gsdsf");
         }
     }
 }
