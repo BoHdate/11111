98fea411139b2e11c2c04d9f1e6b20a9af2adfd9
= <hanten2288@gmail.com>
2020-09-17 12:03:25 +0300

    Add project files.

---
+++ 11111/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _11111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
